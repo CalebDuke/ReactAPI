6173247dca835e37a4547a03b3de02fcd8ac32bc
Caleb Duke <dukecj18@gmail.com>
2020-02-27 10:23:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'isomorphic-fetch';
+import 'es6-promise'
+// import { render } from '@testing-library/react';
 
-function App() {
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+    .then(res => res.json())
+    .then(obj => console.log(obj))
+  }
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +31,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
